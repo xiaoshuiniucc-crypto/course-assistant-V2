0d06587faf9264f2e17bd8dc671d493219630049
xiaoshuiniucc-crypto <xiaoshuiniucc@gmail.com>
2026-05-05 20:58:16 +0800

    Add src/contracts/__init__.py

---
+++ src/contracts/__init__.py
@@ -0,0 +1,9 @@
+"""契约模型模块"""
+from contracts.models import *
+
+__all__ = [
+    "SubmitStatus", "IntentType", "MessageType",
+    "QQMessage", "Courseware", "RubricDimension", "Rubric",
+    "Assignment", "HomeworkSubmission", "GradingResult",
+    "Appeal", "StudentProgress", "TeacherAlert", "IntentResult",
+]
